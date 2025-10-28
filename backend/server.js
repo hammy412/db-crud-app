ca19023eb5c4d6d5993882e26832590c53df8338
Aidan <132329147+hammy412@users.noreply.github.com>
2025-10-28 13:42:05 -0400

    add cors

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const mysql = require('mysql2');
+const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 const db = mysql.createConnection({
   host: process.env.DB_HOST,
