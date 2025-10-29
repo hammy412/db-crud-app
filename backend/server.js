275acab9592854d883c196c23d397dc61a4c3b30
Aidan <132329147+hammy412@users.noreply.github.com>
2025-10-28 20:55:08 -0400

    port

--- backend/server.js
+++ backend/server.js
@@ -92,7 +92,7 @@ app.delete('/items/:id', (req, res) => {
   });
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
 
 module.exports = db;
