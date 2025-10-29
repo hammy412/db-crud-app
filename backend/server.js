12568af7ce29d9e11b73ef52aaa6f8f32ff1ceeb
Aidan <132329147+hammy412@users.noreply.github.com>
2025-10-28 20:48:13 -0400

    cors

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(cors({ origin: '*', methods: ['GET', 'POST', 'PUT', 'DELETE'] }));
 
 const db = mysql.createConnection({
   host: process.env.DB_HOST,
