f41e932c90da87d0ee502c543d391ae91d716db5
Aidan <132329147+hammy412@users.noreply.github.com>
2025-12-01 18:58:43 -0500

    12/1

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,14 @@
 const express = require('express');
 const cors = require('cors');
-const mongoose = require('mongoose');   // <-- you forgot this
+const mongoose = require('mongoose'); 
 require('dotenv').config();
 
 const app = express();
 app.use(express.json());
 app.use(cors({ origin: '*', methods: ['GET', 'POST', 'PUT', 'DELETE'] }));
 
-const Item = require('./schema');  // Mongoose model
+const Item = require('./schema'); 
 
-// CONNECT MONGODB
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => console.log('MongoDB connected'))
